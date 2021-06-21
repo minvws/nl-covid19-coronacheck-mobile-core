09768cff5a1cbe8526935b5dcf9afbbe67aeb0a9
Confiks <confiks@scriptbase.org>
2021-06-21 20:16:08 +0200

    fix: Set ReadEuropeanCredentials expirationTime of specimen EU credentials to a year in the future

--- holder_european.go
+++ holder_european.go
@@ -1,13 +1,23 @@
 package mobilecore
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"time"
+)
 
 func ReadEuropeanCredential(proofPrefixed []byte) *Result {
+	// Read the proof
 	hcert, err := europeanHolder.ReadQREncoded(proofPrefixed)
 	if err != nil {
 		return WrappedErrorResult(err, "Could not read European credential")
 	}
 
+	// If the credential is a specimen, set the expirationTime to a year in the future
+	if hcert.ExpirationTime == HCERT_SPECIMEN_EXPIRATION_TIME {
+		hcert.ExpirationTime = time.Now().Add(365 * 24 * time.Hour).Unix()
+	}
+
+	// Marshal to JSON
 	hcertJson, err := json.Marshal(hcert)
 	if err != nil {
 		return WrappedErrorResult(err, "Could not JSON marshal hcert")
